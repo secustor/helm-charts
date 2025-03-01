66adc32ff7718ea6da25c7ebaa0a5dfc16ecc5ef
Sebastian Poxhofer <secustor@users.noreply.github.com>
2025-03-01 01:08:28 +0100

    ci(release): skip if git tag already exists (#26)

--- scripts/push-charts.ts
+++ scripts/push-charts.ts
@@ -1,71 +1,71 @@
 import * as fs from "node:fs/promises";
 import * as process from "node:process";
 import * as path from "node:path";
-import { exec } from "node:child_process";
+import { execSync } from "node:child_process";
 
 const CR_RELEASE_PACKAGE_PATH = ".cr-release-packages";
+
+interface Archive {
+  path: string;
+  fileName: string;
+}
 
 export async function main(rawArgs: string[]) {
   // remove file path node and script name
   const args = rawArgs.slice(2);
   const archives = await getChangedChartsArchives(args);
 
-  console.log("Pushing charts:");
+  console.log("Pushing charts");
   for (const archive of archives) {
-    console.log(`- ${archive}`);
-    exec(
-      `helm push "${archive}" "oci://ghcr.io/${process.env.GITHUB_REPOSITORY}"`,
-    );
+    const cmd = `helm push "${archive.path}" "oci://ghcr.io/${process.env.GITHUB_REPOSITORY}"`;
+    console.log(cmd);
+    console.log(execSync(cmd).toString());
   }
 }
 
 export async function getChangedChartsArchives(
   args: string[],
-): Promise<string[]> {
-  if (args.length === 0) {
-    console.log("Usage: push-charts <chart-list>");
-    process.exit(1);
-  }
-
-  const changedCharts = parseChartList(args[0]);
+): Promise<Archive[]> {
   const chartArchives = await getChartArchives();
+  const tags = getGitTags();
 
   // translate chart names to chart archives
-  const changedChartArchives: string[] = [];
-  for (const chart of changedCharts) {
-    if (chartArchives[chart]) {
-      const archive = chartArchives[chart];
-      changedChartArchives.push(archive);
+  const changedChartArchives: Archive[] = [];
+  for (const [, value] of Object.entries(chartArchives)) {
+    if (tags.includes(value.fileName)) {
+      // the chart has already pushed
+      console.log(
+        `Skipping ${value.path}. Tag ${value.fileName} already exists`,
+      );
+      continue;
     }
+    changedChartArchives.push(value);
   }
   return changedChartArchives;
 }
 
-function parseChartList(chartListArg: string): string[] {
-  const parsed: string[] = [];
-  for (const chartPath of chartListArg.split(",")) {
-    const name = path.parse(chartPath).name;
-    parsed.push(name);
-  }
-  return parsed;
-}
-
-async function getChartArchives(): Promise<Record<string, string>> {
+async function getChartArchives(): Promise<Record<string, Archive>> {
   const archiveList = await fs.readdir(CR_RELEASE_PACKAGE_PATH, {
     withFileTypes: true,
   });
-  const chartNames: Record<string, string> = {};
+  const chartNames: Record<string, Archive> = {};
   for (const dirent of archiveList) {
     if (dirent.isFile() && dirent.name.endsWith(".tgz")) {
       const parsed = /^(?<name>[\w-]+?)-\d/.exec(dirent.name);
       // immich --> .cr-release-packages/immich-1.0.0.tgz
-      chartNames[parsed.groups.name] = path.join(
-        CR_RELEASE_PACKAGE_PATH,
-        dirent.name,
-      );
+      chartNames[parsed.groups.name] = {
+        path: path.join(CR_RELEASE_PACKAGE_PATH, dirent.name),
+        fileName: path.parse(dirent.name).name,
+      };
     }
   }
   return chartNames;
+}
+
+export function getGitTags(): string[] {
+  const result = execSync("git tag");
+  const tagString = result.toString();
+  return tagString.split("\n");
 }
 
 // do not run main if this script is being tested
